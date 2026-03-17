3ccdb2a0109d3bfb4eebe42b442481d8d0e680ec
Tasin Farhan <tasinfarhan1016@gmail.com>
2026-03-17 13:06:43 +0600

    pactched msvc-demangle for llvm18

--- libiberty/msvc-demangle.cpp
+++ libiberty/msvc-demangle.cpp
@@ -7,40 +7,43 @@
 #include <llvm/Config/llvm-config.h>
 #include <llvm/Demangle/Demangle.h>
 
-extern "C"
-char* msvc_demangle(const char* sym, int options)
-{
+extern "C" char *msvc_demangle(const char *sym, int options) {
   auto mangled = sym;
   auto suffix = std::strchr(sym, '!');
   if (suffix != 0) {
     mangled = suffix + 1;
   }
 
-  auto flags = llvm::MSDemangleFlags(llvm::MSDF_NoAccessSpecifier | llvm::MSDF_NoCallingConvention);
+  auto flags = llvm::MSDemangleFlags(llvm::MSDF_NoAccessSpecifier |
+                                     llvm::MSDF_NoCallingConvention);
   if (!(options & DMGL_ANSI)) {
     /* TODO: Wait for LLVM's demangler to get a flag for this */;
   }
 
-  //HACK: We should probably not do this on DMGL_AUTO, but the GNU C++
-  //      demangler also omits the return type even without
-  //      DMGL_RET_DROP...
+  // HACK: We should probably not do this on DMGL_AUTO, but the GNU C++
+  //       demangler also omits the return type even without
+  //       DMGL_RET_DROP...
   if ((options & (DMGL_RET_DROP | DMGL_AUTO))) {
     flags = llvm::MSDemangleFlags(flags | llvm::MSDF_NoReturnType);
   }
 
-#if LLVM_VERSION_MAJOR > 10
-  auto demangled = llvm::microsoftDemangle(mangled, nullptr, nullptr, nullptr, nullptr, flags);
-#else
-  auto demangled = llvm::microsoftDemangle(mangled, nullptr, nullptr, nullptr, flags);
-#endif
+  #if LLVM_VERSION_MAJOR >= 18
+    auto demangled = llvm::microsoftDemangle(mangled, nullptr, nullptr, (llvm::MSDemangleFlags)flags);
+  #elif LLVM_VERSION_MAJOR > 10
+    auto demangled = llvm::microsoftDemangle(mangled, nullptr, nullptr, nullptr, nullptr, (llvm::MSDemangleFlags)flags);
+  #else
+    auto demangled = llvm::microsoftDemangle(mangled, nullptr, nullptr, nullptr, (llvm::MSDemangleFlags)flags);
+  #endif
+
   if (demangled == nullptr) {
     return nullptr;
   }
 
   auto result = std::string(demangled);
 
-  //TODO: Use OF_NoTagSpecifier (not currently available as a MSDF_* flag)
-  result = std::regex_replace(result, std::regex("(class|struct|union|enum) "), "");
+  // TODO: Use OF_NoTagSpecifier (not currently available as a MSDF_* flag)
+  result =
+      std::regex_replace(result, std::regex("(class|struct|union|enum) "), "");
 
   result = std::regex_replace(result, std::regex(" (\\*|&)"), "$1");
 
